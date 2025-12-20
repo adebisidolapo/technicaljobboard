f928e71aba4dde7c0f62a89fba6f2d6a993abd50
adebisidolapo <121320766+adebisidolapo@users.noreply.github.com>
2025-12-20 14:05:05 +0000

    Fix JSX wrapper issue in page.tsx

--- jobboard/app/page.tsx
+++ jobboard/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
+    <>
+
     <header className="bg-white border-b border-gray-200">
   <div className="max-w-7xl mx-auto px-6 py-6 flex items-center justify-between">
 
@@ -202,5 +204,6 @@ export default function Home() {
 </section>
 
     </main>
+        </>
   );
 }
