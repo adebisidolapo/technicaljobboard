622951d540c3b5b41d8bbb257d93c0c7f9b896e6
James Dave <jamesdaveson001@gmail.com>
2026-03-11 15:51:22 +0100

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import CompanyLogoCarousel from "@/components/CompanyLogoCarousel";
 import FeaturedJobsSection from "@/components/home/FeaturedJobsSection";
