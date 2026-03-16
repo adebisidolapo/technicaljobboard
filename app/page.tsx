60524e0f820cce6c1aa4d09ea0b9b597817f9783
James Dave <jamesdaveson001@gmail.com>
2026-03-16 12:01:26 +0100

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import CompanyLogoCarousel from "@/components/CompanyLogoCarousel";
 import FeaturedJobsSection from "@/components/home/FeaturedJobsSection";
